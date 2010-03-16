c6eeb80906e549caa2bad7d8b59916e09179b1b2
phjgt <phjgt@659e31a5-ba31-4c90-850d-8a4e026bf9ee>
2010-03-16 13:07:29 +0000

    luceneのanalyzer用のコードを追加"
    git-svn-id: http://svn.sourceforge.jp/svnroot/igo/trunk@42 659e31a5-ba31-4c90-850d-8a4e026bf9ee

---
+++ igo-analyzer/src/net/reduls/igo/analysis/IgoAnalyzer.java
@@ -0,0 +1,32 @@
+package net.reduls.igo.analysis;
+
+import java.io.Reader;
+import java.io.IOException;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import net.reduls.igo.Tagger;
+
+public class MorphemeAnalyzer extends Analyzer {
+    private final Tagger tagger;
+
+    public IgoAnalyzer(Tagger tagger) {
+	this.tagger = tagger;
+    }
+
+    @Override
+    public final TokenStream tokenStream(String fieldName, Reader reader) {
+	return new IgoTokenizer(tagger, reader);
+    }
+
+    @Override
+    public final TokenStream reusableTokenStream(String fieldName, Reader reader) throws IOException {
+	IgoTokenizer prev = (IgoTokenizer)getPreviousTokenStream();
+	if(prev == null) {
+	    prev = (IgoTokenizer)tokenStream(fieldName, reader);
+	    setPreviousTokenStream(prev);
+	} else {
+	    prev.reset(reader);
+	}
+	return prev;
+    }
+}
---
+++ igo-analyzer/src/net/reduls/igo/analysis/IgoTokenizer.java
@@ -0,0 +1,85 @@
+package net.reduls.igo.analysis;
+
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.ArrayList;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import net.reduls.igo.Tagger;
+import net.reduls.igo.Morpheme;
+
+public class MorphemeTokenizer extends Tokenizer {
+    private final Tagger tagger;
+    private Iterator<Morpheme> curToken = new ArrayList<Morpheme>().iterator();
+    private int offset = 0;
+    
+    BufferedReader br;
+    
+    private TermAttribute termAtt;
+    private OffsetAttribute offsetAtt;
+    private TypeAttribute typeAtt;
+
+    public IgoTokenizer(Tagger tagger, Reader in) {
+	super(in);
+	this.tagger = tagger;
+	br = new BufferedReader(in);
+
+	termAtt = addAttribute(TermAttribute.class);
+	offsetAtt = addAttribute(OffsetAttribute.class);
+	typeAtt = addAttribute(TypeAttribute.class);
+    }
+
+    @Override
+    public boolean incrementToken() throws IOException {
+	clearAttributes();
+
+	final Morpheme m = readMorpheme();
+	if(m==null)
+	    return false;
+
+	offset = m.start+m.surface.length();
+
+	termAtt.setTermBuffer(m.surface);
+	offsetAtt.setOffset(correctOffset(m.start), correctOffset(offset));
+	typeAtt.setType(m.feature);
+	
+	return true;
+    }
+
+    @Override
+    public final void end() {
+	final int finalOffset = correctOffset(offset);
+	offsetAtt.setOffset(finalOffset, finalOffset);
+    }
+
+    @Override
+    public void reset() throws IOException {
+	super.reset();
+	offset = 0;
+	curToken = new ArrayList<Morpheme>().iterator();
+    }
+
+    @Override
+    public void reset(Reader reader) throws IOException {
+	super.reset(reader);
+	reset();
+	br = new BufferedReader(reader);
+    }
+
+    private Morpheme readMorpheme() throws IOException {
+	if(curToken.hasNext()==false) {
+	    final String line = br.readLine();
+	    if(line==null)
+		return null;
+	    
+	    curToken = tagger.parse(line).iterator();
+	    return readMorpheme();
+	}
+	return curToken.next();
+    }
+}
+
